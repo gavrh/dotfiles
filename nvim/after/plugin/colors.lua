b127931b3032f357b9d832acc8cc3898aa2cb086
Gavin Holmes <96849112+gavrh@users.noreply.github.com>
2025-03-19 16:17:46 -0700

    more changes, new wallpapers, nvidia lag spike fix

--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -8,18 +8,17 @@ function ColorMyPencils(color)
 	hl(0, "Normal", { bg = "none" })
 	hl(0, "NormalFloat", { bg = "none" })
 	hl(0, "SignColumn", { bg = "none" })
-    hl(0, "StatusLine", { bg = "#0a0c0f", fg = "#494c5a" })
-    hl(0, "CursorLine", { bg = "#0a0c0f" })
-    hl(0, "Visual", { bg = "#0a0c0f" })
+	hl(0, "StatusLine", { bg = "#1d202a", fg="#494c5a" })
+    hl(0, "CursorLine", { bg = "#1d202a" })
 
     -- telescope
-    hl(0, "TelescopePromptNormal", { bg = "#21232b" })
-    hl(0, "TelescopeResultsNormal", { bg = "#21232b" })
-    hl(0, "TelescopeResultsBorder", { bg = "none" })
-    hl(0, "TelescopePreviewNormal", { bg = "#21232b" })
+    -- hl(0, "TelescopePromptNormal", { bg = "#21232b" })
+    -- hl(0, "TelescopeResultsNormal", { bg = "#21232b" })
+    -- hl(0, "TelescopeResultsBorder", { bg = "none" })
+    -- hl(0, "TelescopePreviewNormal", { bg = "#21232b" })
 
     -- copilot
-    hl(0, "CopilotSuggestion", { fg = "#494c5a", bg = "none" })
+    -- hl(0, "CopilotSuggestion", { fg = "#494c5a", bg = "none" })
 
 end
 
