cfcfe797b028efc0e155f0343655168b5caeae84
Gavin Holmes <96849112+gavrh@users.noreply.github.com>
2026-08-27 21:13:00 -0700

    i3: fixed autosplit.py and mousecenter.sh

--- i3/autosplit.py
+++ i3/autosplit.py
@@ -1,26 +1,25 @@
 #!/usr/bin/env python3
 
 import i3ipc
-def autosplit(i3, e):
-    tree = i3.get_tree()
-    focused = tree.find_focused()
-    if not focused or not focused.parent:
+
+
+def autosplit(connection, event):
+    focused = connection.get_tree().find_focused()
+    if not focused or focused.type != 'con':
         return
 
-    if focused.parent.layout in ['tabbed', 'stacked']:
+    if focused.floating in ('user_on', 'auto_on'):
         return
 
-    rect = focused.parent.rect
+    parent = focused.parent
+    if not parent or parent.layout in ['tabbed', 'stacked']:
+        return
 
-    if rect.height <= rect.width:
-        i3.command('split horizontal')
-    else:
-        i3.command('split vertical')
+    rect = focused.rect
+    direction = 'horizontal' if rect.width >= rect.height else 'vertical'
+    connection.command(f'[con_id={focused.id}] split {direction}')
 
 i3 = i3ipc.Connection()
 
-i3.on("window::focus", autosplit)
 i3.on("window::new", autosplit)
-i3.on("window::move", autosplit)
-
 i3.main()
